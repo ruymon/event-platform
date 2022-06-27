44e321e821d1fa16259e96d967c73102028100e8
ruymon <ruystfrancis@gmail.com>
2022-06-26 21:27:38 -0300

    :recycle: Add dinamic video

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,12 +1,19 @@
+import { DefaultUi, Player, Youtube } from '@vime/react';
 import styles from './styles.module.css';
+
+import '@vime/core/themes/default.css';
 
 export function Video() {
   return (
     <div className={styles.container}>
       <div className={styles.videoContainer}>
         <div className={styles.videoWrapper}>
-          <div className={styles.mock}>
-          </div>
+          <Player>
+            <Youtube videoId="ouUWJs9dflo"/>
+            <DefaultUi />
+          </Player>
+
+          {/* <div className={styles.mock} /> */}
         </div>
       </div>
     </div>
