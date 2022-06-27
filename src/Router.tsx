5b2316768c3b9ba5a14046652cdbfb958310f528
ruymon <ruystfrancis@gmail.com>
2022-06-26 21:27:58 -0300

    :sparkles: Add routing system

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Event } from "./pages/Event";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Login page - TODO</h1>}/>
+      <Route path="/event" element={<Event />}/>
+      <Route path="*" element={<h1>404</h1>}/>
+    </Routes>
+  )
+}
